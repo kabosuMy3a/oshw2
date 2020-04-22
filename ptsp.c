39bd023d9cf3e10ddcba5bec5db1dc9fc0de7800
kabosuMy3a <my3a3380@gmail.com>
2020-04-22 22:51:25 +0900

    little

--- ptsp.c
+++ ptsp.c
@@ -1,16 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int ** map ;
+int * prefix_arr ;
+int count = 0;
 int size = 0;
-const int * SIZE = &size; 
+int prefix_size = 0;
+int process_bound_at_a_time ;
+const int * SIZE = &size;
+const int * PS = &prefix_size;
+const int * PB = &process_bound_at_a_time ;
+
+
+void terminate_handler(int isg){
+	printf("%d\n", *PS);
+	exit(0);	
+}
+
+void swap(int * a, int * b){
+	int temp = *a;
+	*a = *b;
+	*b= temp;
+}
+
+void parent_routine(int * arr){
+	count ++; 
+	printf("%d| (", count); 
+	for(int i =0 ; i< *PS ; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("%d)\n", arr[0]);
+	return;
+}
+
+void children_routine(){
+	
+}
+
+void (*PR)(int * _arr) = parent_routine ;
+void (*CR)(int * _arr) = children_routine;
+
+void heap_permutation(int * arr, int s, void(*routine)(int * _arr)){	
+	if(s == 1){
+		routine(arr);
+		return; 
+	} 
+	
+	for(int i= 0 ; i < s ; i++){
+		heap_permutation(arr,s-1,routine);
+		if(i< s -1){
+			if(s%2 ==1)
+				swap(&arr[0], &arr[s-1]);
+		} else {
+			swap(&arr[i], &arr[s-1]);
+		}
+	}
+}
+
+void prefix_permutation(){
+	prefix_arr = (int*) malloc(sizeof(int)*(*PS)) ;
+	for(int i = 0 ; i < *PS ; i++)
+		prefix_arr[i] = i;
+	heap_permutation(prefix_arr, *PS, *PR); 	
+}
+
 
 int main(int argc, char** argv){
 
 	if(argc < 3) {
 		fprintf(stderr,"please input file path and the number of processes\n");
 		return -1;
+	}
+	process_bound_at_a_time = atoi(argv[2]) ;
+	if(*PB < 1 || *PB > 12){
+		fprintf(stderr,"please input the number of processes  from 1 to 12\n");
 	}
 	//for knowing size
 	FILE * fp ;
@@ -25,6 +93,11 @@ int main(int argc, char** argv){
 	for(int i =0 ; i<strlen(lines) ; i++){
 		if(lines[i] == ' ' || (lines[i] == '\n' && lines[i-1] != ' ')) size++;
 	}
+	if(*SIZE <13 || *SIZE > 50){
+		fprintf(stderr,"Cities size n should be 13 <= n <= 50\n");
+		return -1;
+	}
+	prefix_size = size - 12 ; 
 	fclose(fp);
 	//until here, for knowing size
 	map = (int **) malloc(sizeof(int*) * (*SIZE));
@@ -39,7 +112,9 @@ int main(int argc, char** argv){
 	}
 	fclose(fp);
 	//until here, for receving input
+	signal(SIGINT,terminate_handler);
 
+	prefix_permutation();
 
 	return 0;
 }
--- tsp17.c
+++ tsp17.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
 
-int m[17][17] ;
+int m[12][12] ;
 
-int path[17] ;
-int used[17] ;
+int path[12] ;
+int used[12] ;
 int length = 0 ;
 int min = -1 ;
+int minpath[12] ;
+int count = 0;
 
 void _travel(int idx) {
 	int i ;
 
-	if (idx == 17) {
-		length += m[path[16]][path[0]] ;
-		//if (min != -1) {
-			//min = length ;
-
-			printf("%d (", length) ;
-			for (i = 0 ; i < 17 ; i++) 
-				printf("%d ", path[i]) ;
-			printf("%d)\n", path[0]) ;	
-		//}
-		length -= m[path[16]][path[0]] ;
+	if (idx == 12) {
+		count ++;
+		length += m[path[11]][path[0]] ;
+		if (min == -1 || min > length ) {
+			min = length ;
+			for(i = 0 ; i< 12 ; i++){
+				minpath[i]= path[i];
+			}
+		}
+		printf("%d| %d (", count, length) ;
+		for (i = 0 ; i < 12 ; i++) 
+			printf("%d ", path[i]) ;
+		printf("%d)\n", path[0]) ;	
+		length -= m[path[11]][path[0]] ;
 	}
 	else {
-		for (i = 0 ; i < 17 ; i++) {
+		for (i = 0 ; i < 12 ; i++) {
 			if (used[i] == 0) {
 				path[idx] = i ;
 				used[i] = 1 ;
@@ -35,6 +41,15 @@ void _travel(int idx) {
 			}
 		}
 	}
+}
+
+void handler(int isg){
+	printf("\nDUDUDUNGA!\n %d (", min) ;
+	for (int i = 0 ; i < 12 ; i++) 
+		printf("%d ", minpath[i]) ;
+	printf("%d) - %d checked\n", minpath[0],count) ;
+	exit(0);
+	
 }
 
 void travel(int start) {
@@ -49,14 +64,20 @@ int main() {
 
 	FILE * fp = fopen("gr17.tsp", "r") ;
 
-	for (i = 0 ; i < 17 ; i++) {
-		for (j = 0 ; j < 17 ; j++) {
+	for (i = 0 ; i < 12 ; i++) {
+		for (j = 0 ; j < 12 ; j++) {
 			fscanf(fp, "%d", &t) ;
 			m[i][j] = t ;
 		}
 	}
 	fclose(fp) ;
+	signal(SIGINT, handler);
 
-	for (i = 0  ; i < 17 ; i++) 
+	for (i = 0  ; i < 12 ; i++) 
 		travel(i) ;
+	printf("\nDUDUDUNGA!\n %d (", min) ;
+	for (int i = 0 ; i < 12 ; i++) 
+		printf("%d ", minpath[i]) ;
+	printf("%d) - %d checked\n", minpath[0],count) ;
+
 }
