d8c3cfeeb424a8bff37e6c6c5a0bf371e417e2cf
kabosuMy3a <my3a3380@gmail.com>
2020-04-23 01:39:56 +0900

    TODO: little

--- ptsp.c
+++ ptsp.c
@@ -5,10 +5,13 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int ** map ;
 int * prefix_arr ;
-int count = 0;
+
+int search_count = 0;
 int size = 0;
 int prefix_size = 0;
 int process_bound_at_a_time ;
@@ -16,10 +19,18 @@ const int * SIZE = &size;
 const int * PS = &prefix_size;
 const int * PB = &process_bound_at_a_time ;
 
+int * children_num;
+int pipes[2] ;
 
-void terminate_handler(int isg){
+void terminate_handler(int sig){
 	printf("%d\n", *PS);
 	exit(0);	
+}
+
+void child_handler(int sig){
+
+	printf("gueeeeegueee\n");
+	exit(0);
 }
 
 void swap(int * a, int * b){
@@ -27,23 +38,6 @@ void swap(int * a, int * b){
 	*a = *b;
 	*b= temp;
 }
-
-void parent_routine(int * arr){
-	count ++; 
-	printf("%d| (", count); 
-	for(int i =0 ; i< *PS ; i++){
-		printf("%d ", arr[i]);
-	}
-	printf("%d)\n", arr[0]);
-	return;
-}
-
-void children_routine(){
-	
-}
-
-void (*PR)(int * _arr) = parent_routine ;
-void (*CR)(int * _arr) = children_routine;
 
 void heap_permutation(int * arr, int s, void(*routine)(int * _arr)){	
 	if(s == 1){
@@ -62,11 +56,62 @@ void heap_permutation(int * arr, int s, void(*routine)(int * _arr)){
 	}
 }
 
-void prefix_permutation(){
+
+void parent_routine(int * arr){
+	if(*children_num < *PB){
+		*children_num += 1;
+		search_count++;
+		printf("%d, %d\n", *children_num, search_count);
+		pid_t child ;
+		child = fork();
+		if(child == 0){
+			key_t key = ftok("shmfile",65);
+			int shmid = shmget(key,1024,0666|IPC_CREAT);
+			children_num = (int*) shmat(shmid,(void*)0,0);
+
+			signal(SIGINT, child_handler);		
+			sleep(2);
+			close(pipes[0]);
+			ssize_t sent = 0;
+			char * data = "If I die tomorrow" ;
+			ssize_t s = strlen(data);
+			//data[s+1] = 0x0;
+			while(sent < s){
+				sent += write(pipes[1], data+sent, s-sent);	
+			}
+			*children_num -= 1;
+			shmdt(children_num);
+			close(pipes[1]);
+			exit(0);	
+		}
+  
+	} else {
+		while(*children_num >= *PB){
+		}
+		parent_routine(arr);
+	} 
+}
+
+void children_routine(int * arr){
+	search_count ++; 
+	printf("%d| (", search_count); 
+	for(int i =0 ; i< *PS ; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("%d)\n", arr[0]);
+	return;
+}
+
+void (*PARENT_ROUTINE)(int * _arr) = parent_routine ;
+void (*CHILDEREN_ROUTINE)(int * _arr) = children_routine;
+
+
+
+void start(){
 	prefix_arr = (int*) malloc(sizeof(int)*(*PS)) ;
 	for(int i = 0 ; i < *PS ; i++)
 		prefix_arr[i] = i;
-	heap_permutation(prefix_arr, *PS, *PR); 	
+	heap_permutation(prefix_arr, *PS, *PARENT_ROUTINE); 	
 }
 
 
@@ -97,6 +142,7 @@ int main(int argc, char** argv){
 		fprintf(stderr,"Cities size n should be 13 <= n <= 50\n");
 		return -1;
 	}
+	
 	prefix_size = size - 12 ; 
 	fclose(fp);
 	//until here, for knowing size
@@ -114,7 +160,33 @@ int main(int argc, char** argv){
 	//until here, for receving input
 	signal(SIGINT,terminate_handler);
 
-	prefix_permutation();
+	if(pipe(pipes) != 0){
+		fprintf(stderr,"Pipe Create Error\n");
+	}
+	
+	key_t key = ftok("shmfile",65);
+	int shmid = shmget(key,1024,0666|IPC_CREAT);
+	children_num = (int*) shmat(shmid,(void*)0,0);
+	*children_num = 0;
 
+	pid_t slave ;
+	int exit_code;
+	slave = fork();
+	if(slave==0){
+		start();
+	} else /*master*/{
+		char buf[32] ;
+		ssize_t s;
+		close(pipes[1]);
+		while(1){	
+			while((s = read(pipes[0], buf, 31))> 0){
+				buf[s+1] = 0x0;
+				printf("%s\n",buf);
+			}
+		}	
+		close(pipes[0]);	
+	}
+	wait(&exit_code);
+	
 	return 0;
 }
